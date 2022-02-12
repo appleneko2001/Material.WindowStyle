ebc9fdda88a5a7cec4b486dc8a416fce0ecb9740
appleneko2001 <appleneko2001@outlook.com>
2022-02-12 14:35:41 +0900

    Use namespace block instead of new-style

--- src/Material.WindowStyle/Assists/MaterialTitleBarButtonAssist.cs
+++ src/Material.WindowStyle/Assists/MaterialTitleBarButtonAssist.cs
@@ -2,18 +2,19 @@
 using Avalonia.Controls;
 using Avalonia.Media;
 
-namespace Material.WindowStyle.Assists;
-
-public static class MaterialTitleBarButtonAssist
+namespace Material.WindowStyle.Assists
 {
-    public static readonly AvaloniaProperty<IBrush> OnHoverProperty = AvaloniaProperty.RegisterAttached<Button, IBrush>(
-        "OnHover", typeof(MaterialTitleBarButtonAssist));
+    public static class MaterialTitleBarButtonAssist
+    {
+        public static readonly AvaloniaProperty<IBrush> OnHoverProperty = AvaloniaProperty.RegisterAttached<Button, IBrush>(
+            "OnHover", typeof(MaterialTitleBarButtonAssist));
 
-    public static IBrush GetOnHover(AvaloniaObject element) {
-        return (IBrush) element.GetValue(OnHoverProperty);
-    }
+        public static IBrush GetOnHover(AvaloniaObject element) {
+            return (IBrush) element.GetValue(OnHoverProperty);
+        }
 
-    public static void SetOnHover(AvaloniaObject element, IBrush value) {
-        element.SetValue(OnHoverProperty, value);
+        public static void SetOnHover(AvaloniaObject element, IBrush value) {
+            element.SetValue(OnHoverProperty, value);
+        }
     }
 }
