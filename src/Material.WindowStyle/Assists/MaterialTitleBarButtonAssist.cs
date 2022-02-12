3ab4d0e532a3e52aa1919591f7e71f3d4dd18e65
appleneko2001 <appleneko2001@outlook.com>
2022-02-12 14:20:05 +0900

    Add assist for title bar button

---
+++ src/Material.WindowStyle/Assists/MaterialTitleBarButtonAssist.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Material.WindowStyle.Assists;
+
+public static class MaterialTitleBarButtonAssist
+{
+    public static readonly AvaloniaProperty<IBrush> OnHoverProperty = AvaloniaProperty.RegisterAttached<Button, IBrush>(
+        "OnHover", typeof(MaterialTitleBarButtonAssist));
+
+    public static IBrush GetOnHover(AvaloniaObject element) {
+        return (IBrush) element.GetValue(OnHoverProperty);
+    }
+
+    public static void SetOnHover(AvaloniaObject element, IBrush value) {
+        element.SetValue(OnHoverProperty, value);
+    }
+}
