fa35fb5a1906682e5c902f932de880a58235d529
appleneko2001 <appleneko2001@outlook.com>
2022-08-25 23:19:59 +0900

    Update main view model

--- src/Material.WindowStyle.Demo/Views/ViewModels/DemoWindowViewModel.cs
+++ src/Material.WindowStyle.Demo/Views/ViewModels/DemoWindowViewModel.cs
@@ -26,28 +26,56 @@ namespace Material.WindowStyle.Demo.Views.ViewModels
         };
         public AvaloniaList<EntityViewModel> OptionalSupportedLibs { get; } = new()
         {
-            new BulletItemEntityViewModel
+            new BulletItemEntityViewModel(new EntityViewModel []
             {
-                Entities =
+                new LinkEntityViewModel("Material.Avalonia toolkit")
                 {
-                    new LinkEntityViewModel("Material.Avalonia toolkit")
-                    {
-                        Href = "https://github.com/AvaloniaCommunity/Material.Avalonia",
-                        ToolTip = "This toolkit is a collection of styles to help you customize your Avalonia application theme with Material Design."
-                    },
-                    new TextEntityViewModel(" version 2.4.0 or newer.")
-                }
-            }
+                    Href = "https://github.com/AvaloniaCommunity/Material.Avalonia",
+                    ToolTip = "This toolkit is a collection of styles to help you customize your Avalonia application theme with Material Design."
+                },
+                new TextEntityViewModel(" version 2.4.0 or newer.")
+            })
         };
         public AvaloniaList<EntityViewModel> Steps { get; } = new()
         {
+            new BulletItemEntityViewModel(new []
+            {
+                new TextEntityViewModel("Update your Avalonia UI framework to latest. Make sure your framework version is newer or equal 11.0.0-preview1.")
+            }),
+            new BulletItemEntityViewModel(new []
+            {
+                new TextEntityViewModel("Install Material.WindowStyle package from NuGet. The major and minor package version should be 0.11. Build and revision version should be as newer as possible so you wont miss updates.")
+            }),
+            new BulletItemEntityViewModel(new []
+            {
+                new TextEntityViewModel("Add "),
+                new MonospaceTextEntityViewModel("<StyleInclude Source=\"avares://Material.WindowStyle/WindowStyleInclude.axaml\"/>"),
+                new TextEntityViewModel(" to your styles of Application. It should be after theme styles and before your custom styles.")
+            }),
+            new BulletItemEntityViewModel(new []
+            {
+                new TextEntityViewModel("Add those attributes to your "),
+                new MonospaceTextEntityViewModel("Window"),
+                new TextEntityViewModel(" view:")
+            }),
+            new MonospaceTextEntityViewModel("ExtendClientAreaToDecorationsHint=\"True\""),
+            new BulletItemEntityViewModel(new []
+            {
+                new TextEntityViewModel("(Optional) You may want apply fix for Windows Platform. This fix used for correcting margin while the Window is maximized. Windows OS could overfill the window."),
+                new TextEntityViewModel("To apply this fix, just add "),
+                new MonospaceTextEntityViewModel("windows-platform-fix"),
+                new TextEntityViewModel(" to your "),
+                new MonospaceTextEntityViewModel("Classes"),
+                new TextEntityViewModel(" property of window. This can be done automatically or manually, depending how you create your avalonia application.  ")
+            }),
+            
             new TextEntityViewModel("This area is not ready!")
             {
                 FontStyle = FontStyle.Italic
             }
         };
 
-        private string _source;
+        private string _source = string.Empty;
         public string Source
         {
             get => _source;
